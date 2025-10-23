131159ac06c23fb14d17a4f84829b04605d9219f
Yves Ndoky <scorpiondoky@gmail.com>
2025-10-22 20:03:50 -0400

    Acitivité01 effectuée

--- src/orders/commands/write_order.py
+++ src/orders/commands/write_order.py
@@ -114,8 +114,11 @@ def request_payment_link(order_id, total_amount, user_id):
 
     # TODO: Requête à POST /payments
     print("")
-    response_from_payment_service = {}
-
+    response_from_payment_service = requests.post('/payments-api/payments',
+      json=payment_transaction,
+      headers={'Content-Type': 'application/json'}
+  )
+    payment_id = response_from_payment_service.json().get("payment_id")
     if True: # if response.ok
         print(f"ID paiement: {payment_id}")
 
